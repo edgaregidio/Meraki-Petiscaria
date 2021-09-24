5580ffc197eb5165cf38b96598b3eda2375c4d99
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2021-09-24 11:07:26 -0300

    fix: load images to items

--- components/Products/index.js
+++ components/Products/index.js
@@ -1,143 +1,132 @@
-import React from 'react'
-import logob from '../../public/logob.png'
-import Image from 'next/dist/client/image'
-import {
-  ProductList,
-  Line2,
-  LinePrice,
-  Circle,
-  // ImageProduct
-} from './styles'
+import React from "react";
+import { ProductList, Line2, LinePrice, Circle, ImageProduct } from "./styles";
 
-export default function Products() {
-
+export default function product() {
   const data = [
     {
       id: 1,
-      title: 'Batata Frita',
-      descripition: '',
-      price: '27,00',
-      img: require('../../public/logob.png'),
-      details: '500g',
+      title: "Batata Frita",
+      descripition: "",
+      price: "27,00",
+      img: "/logob.png",
+      details: "500g",
     },
     {
       id: 2,
-      title: 'Batata Cheddar e Bacon 500g',
-      descripition: '',
-      price: '32,00',
-      img: '',
-      details: '500g',
+      title: "Batata Cheddar e Bacon 500g",
+      descripition: "",
+      price: "32,00",
+      img: "/logob.png",
+      details: "500g",
     },
     {
       id: 3,
-      title: 'Bolinho de costela suina',
-      descripition: 'Bolinho de costela suina com Barbecue',
-      price: '36,00',
-      img: require('../../public/logob.png'),
-      details: '10ud',
+      title: "Bolinho de costela suina",
+      descripition: "Bolinho de costela suina com Barbecue",
+      price: "36,00",
+      img: "/logob.png",
+      details: "10ud",
     },
     {
       id: 4,
-      title: 'Bolinho de Tilápia',
-      descripition: 'Recheado com queijo e alcaparras.',
-      price: '38,00',
-      img: '',
-      details: '',
+      title: "Bolinho de Tilápia",
+      descripition: "Recheado com queijo e alcaparras.",
+      price: "38,00",
+      img: "/logob.png",
+      details: "",
     },
     {
       id: 5,
-      title: 'Bolinho de Mandioca',
-      descripition: 'Bolinho de mandioca com carne seca.',
-      price: '35,00',
-      img: '',
-      details: '10ud',
+      title: "Bolinho de Mandioca",
+      descripition: "Bolinho de mandioca com carne seca.",
+      price: "35,00",
+      img: "/logob.png",
+      details: "10ud",
     },
     {
       id: 6,
-      title: 'Disco de Costela',
-      descripition: 'Com cheddar e bacon.',
-      price: '38,00',
-      img: '',
-      details: '10ud',
+      title: "Disco de Costela",
+      descripition: "Com cheddar e bacon.",
+      price: "38,00",
+      img: "/logob.png",
+      details: "10ud",
     },
     {
       id: 7,
-      title: 'Disco de Carne',
-      descripition: 'Disco de carne recheado com queijo.',
-      price: '37,00',
-      img: '',
-      details: '10ud',
+      title: "Disco de Carne",
+      descripition: "Disco de carne recheado com queijo.",
+      price: "37,00",
+      img: "/logob.png",
+      details: "10ud",
     },
     {
       id: 8,
-      title: 'Frango a Kiev',
-      descripition: 'Recheado com provolone.',
-      price: '48,00',
-      img: '',
-      details: '600g',
+      title: "Frango a Kiev",
+      descripition: "Recheado com provolone.",
+      price: "48,00",
+      img: "/logob.png",
+      details: "600g",
     },
     {
       id: 9,
-      title: 'Isca de franco Empanada',
-      descripition: '',
-      price: '38,00',
-      img: '',
-      details: '10ud',
+      title: "Isca de franco Empanada",
+      descripition: "",
+      price: "38,00",
+      img: "/logob.png",
+      details: "10ud",
     },
     {
       id: 10,
-      title: 'Palitinho Mineiro',
-      descripition: 'Provolone empanado.',
-      price: '38,00',
-      img: '',
-      details: '10ud',
+      title: "Palitinho Mineiro",
+      descripition: "Provolone empanado.",
+      price: "38,00",
+      img: "/logob.png",
+      details: "10ud",
     },
     {
       id: 11,
-      title: 'Pastel de carne',
-      descripition: 'Recheado com queijo.',
-      price: '32,00',
-      img: '',
-      details: '10ud',
+      title: "Pastel de carne",
+      descripition: "Recheado com queijo.",
+      price: "32,00",
+      img: "/logob.png",
+      details: "10ud",
     },
     {
       id: 12,
-      title: '',
-      descripition: '',
-      price: '35,00',
-      img: '',
-      details: '10ud',
+      title: "",
+      descripition: "",
+      price: "35,00",
+      img: "/logob.png",
+      details: "10ud",
     },
-
-  ]
-  console.log(data.img)
+  ];
 
   return (
     <div>
-      {
-        data.map(products =>
-          <ProductList key={products.id}>
-            <h1>{products.title}</h1>
-            <p>{products.descripition}</p>
+      {data.map((product) => {
+        return (
+          <ProductList key={product.id}>
+            <h1>{product.title}</h1>
+            <p>{product.descripition}</p>
             <Line2>
               <LinePrice>
                 <h4>R$</h4>
-                <h3>{products.price}</h3>
+                <h3>{product.price}</h3>
               </LinePrice>
               <Circle>
-                <h1>{products.details}</h1>
+                <h1>{product.details}</h1>
               </Circle>
             </Line2>
-            <Image
-            src={products.img}
-            alt="Imagem do produto"
-            layout="responsive"
-            width={150}
-            height={70} />
+            <ImageProduct
+              alt="Imagem do produto"
+              layout="responsive"
+              width={150}
+              height={70}
+              src={product.img}
+            />
           </ProductList>
-        )
-      }
-
+        );
+      })}
     </div>
-  )
+  );
 }
