18578d2b089ba1399b32f09b0e1909db212f8af0
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2021-09-28 19:52:49 -0300

    generate static site

---
+++ _static/_next/static/Jv_5VyUPy4mXDd8MUjgQ8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/706-2f6e1f2f7af678d33439.js","static/chunks/pages/index-d2a5a4817b7929319512.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
